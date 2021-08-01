059adac9334773ccc956d2f2d03c28ece741edf0
vr009 <slavarianov@yandex.ru>
2021-08-02 01:10:29 +0300

    message model done

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,7 +1,16 @@
 package models
 
+import "time"
+
 type User struct {
 	//Id       string `json:"uuid"`
 	UserName string `json:"UserName"`
 	Password string `json:"Password"`
+}
+
+type message struct {
+	Body      string
+	Direction User
+	Sender    User
+	Sent      time.Time
 }
